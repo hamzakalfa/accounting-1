22528a80fb435c27e38afbbbb6efe3b0bd455d08
asriMOHSSIN <34395009+asriMOHSSIN@users.noreply.github.com>
2019-06-10 15:41:16 +0100

    Update createaccount.php

--- createaccount.php
+++ createaccount.php
@@ -3,11 +3,11 @@ $dbhost = getenv("MYSQL_SERVICE_HOST");
 $dbport = getenv("MYSQL_SERVICE_PORT");
 $dbuser = getenv("databaseuser");
 $dbpwd = getenv("databasepassword");
-$dbname = getenv("databasename");
+$dbname = getenv("sampledb");
 //Database operations
 //db_createaccount
-//$connection = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
-$connection = new mysqli("localhost","root","","db_createaccount");
+$connection = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
+//$connection = new mysqli("localhost","root","","db_createaccount");
 if ($connection->connect_errno) {
     printf("Connect failed: %s\n", $mysqli->connect_error);
     exit();
