e4501598815e223a25e00e4e14c1ba3a0c81de87
asriMOHSSIN <34395009+asriMOHSSIN@users.noreply.github.com>
2019-06-10 15:35:01 +0100

    Add files via upload

---
+++ createaccount.php
@@ -0,0 +1,39 @@
+<?php
+$dbhost = getenv("MYSQL_SERVICE_HOST");
+$dbport = getenv("MYSQL_SERVICE_PORT");
+$dbuser = getenv("databaseuser");
+$dbpwd = getenv("databasepassword");
+$dbname = getenv("databasename");
+//Database operations
+//db_createaccount
+//$connection = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
+$connection = new mysqli("localhost","root","","db_createaccount");
+if ($connection->connect_errno) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+} else {
+    printf("Connected to the database<br>");
+}
+//$nom_Client=$_GET["nom"];
+//$prenom_Client=$_GET["prenom"];
+//$email=$_GET["email"];
+//$address=$_GET["address"];
+//$phone_number=$_GET["tel"];
+//$decision=$_GET["decision"];
+//$dateofBirth=$_GET["daten"];
+//$zipcode=$_GET["zipcode"];
+//$education=$_GET["education"];
+//$status=$_GET["status"];
+//$conditionsAcceptance=$_GET["ca"];
+
+$sql = "INSERT INTO `client`(`conditions_acceptance`, `dateof_birth`, `decision`, `education`, `status`, `zipcode`, `address`, `email`,`nom_client`, `phone_number`, `prenom_client`) VALUES (true,'2019-05-14T22:00:00.000+0000','decision','education','celib',666666,'addr','hhh@hhhh.com','nom12344455','0678869500','prenom');
+";
+
+
+if ($connection->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $connection->error;
+}
+$connection->close();
+?>
---
+++ getclient.php
@@ -0,0 +1,34 @@
+ <?php
+$dbhost = getenv("MYSQL_SERVICE_HOST");
+$dbport = getenv("MYSQL_SERVICE_PORT");
+$dbuser = getenv("databaseuser");
+$dbpwd = getenv("databasepassword");
+$dbname = getenv("databasename");
+//Database operations
+//db_createaccount
+//$connection = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
+$connection = new mysqli("localhost","root","","db_createaccount");
+if ($connection->connect_errno) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+} else {
+    printf("Connected to the database<br>");
+}
+
+$sql = "SELECT * from client";
+$result = $connection->query($sql);
+$data=array();
+if ($result->num_rows > 0) {
+	
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+		print json_encode($row);
+        //echo "{\"client\"<br>id : " . $row["id_client"]. " - Name: " . $row["prenom_client"]. " " . $row["nom_client"]. "<br>";
+		
+    }
+	print json_encode($data);
+} else {
+    echo "0 results";
+}
+$connection->close();
+?> 
